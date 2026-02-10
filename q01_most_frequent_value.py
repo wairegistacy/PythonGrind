2e63fe5841946917c280b469664e3b3147bdf57a
StacyWairegi <48006331+wairegistacy@users.noreply.github.com>
2026-02-10 23:43:44 +0000

    Delete q01_most_frequent_value.py

--- q01_most_frequent_value.py
+++
@@ -1,7 +0,0 @@
-data = {"a": "apple", "b": "banana", "c": "apple", "d": "orange", "e": "apple"}
-counts = {}
-for value in data.values():
-    counts[value] = counts.get(value, 0)+ 1
-
-max_value = max(counts, key=counts.get)
-print(max_value)
