bb6aa74e4c4d9572db019f9773d1191004fde04a
StacyWairegi <48006331+wairegistacy@users.noreply.github.com>
2026-03-13 18:50:33 +0000

    General refactors
    * refactors
    
    * updating to use elif

--- guessing_game.py
+++ guessing_game.py
@@ -2,22 +2,25 @@ import random
 
 def guessing_game():
     # the random no generated from 1 to 10
-    answer = random.randint(1,10)
+    answer = random.randint(1, 10)
     count = 0
+
     while answer < 10:
-        gues = int(input('Guess a number from 1 to 10: '))
-        if gues < 1 or gues > 10:
+        guess = int(input('Guess a number from 1 to 10: '))
+
+        if guess < 1 or guess > 10:
             print('Please enter a number between 1 and 10')
             continue
-        if gues > answer:
-            print ('Too high! Guess again')
-        if gues < answer:
-            print ('Too low! Guess again')
-        if gues == answer:
-            print ('Correct!')
+
+        if guess == answer:
+            print('Correct!')
             break
+        elif guess > answer:
+            print ('Too high! Guess again')
+        else:
+            print ('Too low! Guess again')
         
         count += 1
 
-#call function to call the game
+# call function to call the game
 guessing_game()
