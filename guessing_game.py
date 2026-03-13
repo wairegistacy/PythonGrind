daf9c51dcddf97759a33e712425ed496f32e907f
wairegistacy <stacywairegi96@gmail.com>
2026-03-13 18:31:12 +0000

    refactors

--- guessing_game.py
+++ guessing_game.py
@@ -2,22 +2,24 @@ import random
 
 def guessing_game():
     # the random no generated from 1 to 10
-    answer = random.randint(1,10)
+    answer = random.randint(1, 10)
     count = 0
+
     while answer < 10:
-        gues = int(input('Guess a number from 1 to 10: '))
-        if gues < 1 or gues > 10:
+        guess = int(input('Guess a number from 1 to 10: '))
+
+        if guess < 1 or guess > 10:
             print('Please enter a number between 1 and 10')
             continue
-        if gues > answer:
+        if guess > answer:
             print ('Too high! Guess again')
-        if gues < answer:
+        if guess < answer:
             print ('Too low! Guess again')
-        if gues == answer:
+        if guess == answer:
             print ('Correct!')
             break
         
         count += 1
 
-#call function to call the game
+# call function to call the game
 guessing_game()
